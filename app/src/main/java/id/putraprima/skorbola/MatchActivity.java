94c269c81685106fb4ae9fc55d4fac96740dd303
Shikirashi <65103461+Shikirashi@users.noreply.github.com>
2022-11-08 12:44:05 +0700

    Initial commit

--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,14 +2,96 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MatchActivity extends AppCompatActivity {
+
+    private String hometeam;
+    private String awayteam;
+    private String winner;
+    private int homescore;
+    private int awayscore;
+    private TextView scoreHome;
+    private TextView scoreAway;
+    private TextView homeText;
+    private TextView awayText;
+    private ImageView homeLogo;
+    private ImageView awayLogo;
+    private Button addHome;
+    private Button addAway;
+    private Button cekResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+        homeText = findViewById(R.id.txt_home);
+        awayText = findViewById(R.id.txt_away);
+        scoreHome = findViewById(R.id.score_home);
+        scoreAway = findViewById(R.id.score_away);
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+        addHome = findViewById(R.id.btn_add_home);
+        addAway = findViewById(R.id.btn_add_away);
+        cekResult = findViewById(R.id.btn_result);
+
+        homescore = 0;
+        awayscore = 0;
+        scoreHome.setText(String.valueOf(homescore));
+        scoreAway.setText(String.valueOf(awayscore));
+
+        Bundle bundle = getIntent().getExtras();
+        hometeam = bundle.getString("namahome");
+        homeText.setText(hometeam);
+        awayteam = bundle.getString("namaaway");
+        awayText.setText(awayteam);
+        homeLogo.setImageURI(Uri.parse(bundle.getString("homeImg")));
+        awayLogo.setImageURI(Uri.parse(bundle.getString("awayImg")));
+
+        addHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homescore += 1;
+                scoreHome.setText(String.valueOf(homescore));
+            }
+        });
+
+        addAway.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                awayscore += 1;
+                scoreAway.setText(String.valueOf(awayscore));
+            }
+        });
+
+        cekResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                winner = "empty";
+                if(homescore > awayscore){
+                    winner = hometeam;
+                }
+                else if (homescore == awayscore){
+                    winner = "draw";
+                }
+                else {
+                    winner = awayteam;
+                }
+
+                Intent intent = new Intent(MatchActivity.this, ResultActivity.class);
+                intent.putExtra("winner", winner);
+                startActivity(intent);
+            }
+        });
+
+
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
         //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
